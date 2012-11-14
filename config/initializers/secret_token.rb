707274851b9ef50cd55dd3577b8ebf253c55c0a2
raymond alvarez <nextwaveray@gmail.com>
2012-11-14 10:55:11 -0700

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruitstand::Application.config.secret_token = '237dbc9bab53ec925dbeb47975161e02b4ae1755e9d3f08e1448a9d794740e75aa3b004c3cdb99e581f7ae688c9c6429d46fd7cf730656bbd49155321a4feb73'
